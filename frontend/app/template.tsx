afef7d805991e31fcf7f3e910711184e52436032
vinh <lequocvinh140213@gmail.com>
2024-01-31 08:36:32 +0700

    apply hook

--- frontend/app/template.tsx
+++ frontend/app/template.tsx
@@ -1,37 +1,16 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import HeaderDefault from "~/components/layouts/Header";
+import { useSelector } from "react-redux";
+import HeaderDefault from "~/components/layouts/header/Header";
 import LoginPage from "~/components/layouts/login/LoginPage";
+import { useAuthentication } from "~/hooks/use-authentication";
+import { usePush } from "~/hooks/use-push";
 import { RootState } from "~/redux/store";
-import { useDispatch, useSelector } from "react-redux";
-import Cookie from "js-cookie";
-import { useGet } from "~/hooks/use-api";
-import { setAuthentication, setAuth } from "~/redux/slices/auth.slice";
-import { Builder } from "builder-pattern";
-import { Auth } from "~/interface/auth.interface";
-import { User } from "~/interface/user.interface";
-import { setUser } from "~/redux/slices/user.slice";
 
 const Template = ({ children }: { children: React.ReactNode }) => {
   const auth = useSelector((state: RootState) => state.auth);
-  const dispatch = useDispatch();
-  const getAuth = useGet("auth", { alert: { isUsed: false } });
-
-  useEffect(() => {
-    const token = Cookie.get("access");
-    if (token) {
-      const getCurrentUser = async () => {
-        dispatch(setAuthentication(token));
-        try {
-          const user = (await getAuth("current-user")) as User;
-          dispatch(setAuth(Builder<Auth>().user(user).jwt(token).build()));
-          dispatch(setUser(user));
-        } catch (error) {}
-      };
-      getCurrentUser();
-    }
-  }, [dispatch, auth.jwt]);
+  usePush();
+  useAuthentication();
 
   return auth.isAuthenticated ? (
     <>
